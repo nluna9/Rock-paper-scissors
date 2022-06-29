8a5555f7c01a86a11d09b47ac05b07d820a556ac
Nicolas Luna <nicolasluna379@gmail.com>
2022-06-28 20:04:59 -0500

    function singleRound now is case-insensitive and request a valid option in case is not a string or isn-t any of the 3 valid options

--- main.js
+++ main.js
@@ -7,14 +7,16 @@ function computerPlay(){
 
 
 function singleRound(playerSelection, computerSelection = computerPlay()){
-    if(playerSelection === computerSelection){
-        return 'You draw';
-    } else if(words.indexOf(playerSelection)- words.indexOf(computerSelection) == 1 || words.indexOf(playerSelection)- words.indexOf(computerSelection) == 2  ){
-        return 'You win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + ' beats ' + computerSelection;
+    if (typeof playerSelection === 'string' && words.includes(playerSelection.toLowerCase())){
+        playerSelection = playerSelection.toLowerCase();
+          if(playerSelection === computerSelection){
+              return 'You draw';
+          } else if(words.indexOf(playerSelection)- words.indexOf(computerSelection) == 1 || words.indexOf(playerSelection)- words.indexOf(computerSelection) == -2  ){
+        return 'You win! ' + playerSelection[0].toUpperCase() + playerSelection.slice(1) + ' beats ' + computerSelection + '.';
     } else {
-        return 'You lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + ' beats ' + playerSelection;
-    }
+        return 'You lose! ' + computerSelection[0].toUpperCase() + computerSelection.slice(1) + ' beats ' + playerSelection + '.';
+    }}
 }
 
 console.log(computerPlay());
-console.log(singleRound('paper'));
+console.log(singleRound('pAPer'));
