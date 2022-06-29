39d71d131d266ba3f0fab0769c5cfa5d681dd302
Nicolas Luna <nicolasluna379@gmail.com>
2022-06-28 19:04:56 -0500

    function singleRound done

--- main.js
+++ main.js
@@ -5,4 +5,16 @@ function computerPlay(){
     return words[rand]
 }
 
+
+function singleRound(playerSelection, computerSelection = computerPlay()){
+    if(playerSelection === computerSelection){
+        return 'You draw';
+    } else if(words.indexOf(playerSelection)- words.indexOf(computerSelection) == 1 || words.indexOf(playerSelection)- words.indexOf(computerSelection) == 2  ){
+        return 'You won!, ' + playerSelection + ' beats ' + computerSelection;
+    } else {
+        return 'You lost!, ' + computerSelection + ' beats ' + playerSelection;
+    }
+}
+
 console.log(computerPlay());
+console.log(singleRound('paper'));
